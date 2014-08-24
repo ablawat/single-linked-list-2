d588e02d3d614d556b1c373f05438bee1a5693ec
Adam Bławat <blawat.adam@gmail.com>
2014-08-24 13:20:20 +0200

    Dodałem funkcje testujące.

--- test.c
+++ test.c
@@ -1,69 +1,91 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 #include "data.h"
-#include "linked-list-int.h"
+#include "linked-list-data.h"
+
+void list_create_test()
+{
+    linked_list_data_t **list = linked_list_data_create();
+    
+    assert(list[0] == NULL);
+    assert(list[1] == NULL);
+}
+
+void list_add_test()
+{
+    linked_list_data_t **list;
+    
+    data_t *data1;
+    data_t *data2;
+    
+    data1 = malloc(sizeof(data_t));
+    data1 -> value1 = 1;
+    data1 -> value2 = 2;
+    
+    data2 = malloc(sizeof(data_t));
+    data2 -> value1 = 3;
+    data2 -> value2 = 4;
+    
+    
+    list = linked_list_data_create();
+    
+    linked_list_data_add_first(list, data1);
+    
+    assert(list[0] == list[1]);
+    assert(list[0] != NULL);
+    assert(list[0] -> value == data1);
+    assert(list[0] -> next  == NULL);
+    
+    assert(list[0] -> value -> value1 == 1);
+    assert(list[0] -> value -> value2 == 2);
+    
+    linked_list_data_add_first(list, data2);
+    
+    assert(list[0] != NULL);
+    assert(list[1] != NULL);
+    assert(list[0] != list[1]);
+    
+    assert(list[0] -> value == data2);
+    assert(list[0] -> next  == list[1]);
+    assert(list[1] -> value == data1);
+    assert(list[1] -> next  == NULL);
+    
+    assert(list[0] -> value -> value1 == 3);
+    assert(list[0] -> value -> value2 == 4);
+    
+    
+    list = linked_list_data_create();
+    
+    linked_list_data_add_last(list, data1);
+    
+    assert(list[0] == list[1]);
+    assert(list[0] != NULL);
+    assert(list[0] -> value == data1);
+    assert(list[0] -> next  == NULL);
+    
+    assert(list[0] -> value -> value1 == 1);
+    assert(list[0] -> value -> value2 == 2);
+    
+    linked_list_data_add_last(list, data2);
+    
+    assert(list[0] != NULL);
+    assert(list[1] != NULL);
+    assert(list[0] != list[1]);
+    
+    assert(list[0] -> value == data1);
+    assert(list[0] -> next  == list[1]);
+    assert(list[1] -> value == data2);
+    assert(list[1] -> next  == NULL);
+    
+    assert(list[1] -> value -> value1 == 3);
+    assert(list[1] -> value -> value2 == 4);
+}
 
 int main()
 {
-    data_t *value;
-    
-    linked_list_data_t **list = linked_list_data_create();
-    
-    linked_list_data_print(list);
-    putchar(10);
-    
-    value = malloc(sizeof(data_t));
-    value -> value1 = 1;
-    value -> value2 = 2;
-    linked_list_data_add_first(list, value);
-    linked_list_data_print(list);
-    putchar(10);
-    
-    value = malloc(sizeof(data_t));
-    value -> value1 = 3;
-    value -> value2 = 4;
-    linked_list_data_add_first(list, value);
-    linked_list_data_print(list);
-    putchar(10);
-    
-    value = malloc(sizeof(data_t));
-    value -> value1 = 5;
-    value -> value2 = 6;
-    linked_list_data_add_last(list, value);
-    linked_list_data_print(list);
-    putchar(10);
-    
-    value = malloc(sizeof(data_t));
-    value -> value1 = 1;
-    value -> value2 = 2;
-    linked_list_data_add_first(list, value);
-    linked_list_data_print(list);
-    putchar(10);
-    
-    linked_list_data_clear(list);
-    linked_list_data_print(list);
-    putchar(10);
-
-    value = malloc(sizeof(data_t));
-    value -> value1 = 7;
-    value -> value2 = 8;
-    linked_list_data_add_last(list, value);
-    linked_list_data_print(list);
-    putchar(10);
-
-    value = malloc(sizeof(data_t));
-    value -> value1 = 3;
-    value -> value2 = 4;
-    linked_list_data_add_last(list, value);
-    linked_list_data_print(list);
-    putchar(10);
-
-    value = malloc(sizeof(data_t));
-    value -> value1 = 1;
-    value -> value2 = 2;
-    linked_list_data_add_first(list, value);
-    linked_list_data_print(list);
-    putchar(10);
+    list_create_test();
+    list_add_test();
     
     return 0;
 }
